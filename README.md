c324d6e8d9d544c42e0f2145408830a398a00e6b
Joah Gerstenberg <joahd@gbergfamily.com>
2013-12-06 11:15:29 -0600

    Init commit - work so far

---
+++ README.md
@@ -0,0 +1,12 @@
+#gh.sh
+
+Bash shell script to enable command line GitHub interaction.
+
+---
+
+To use, replace `$GH_AUTH_TOKEN` with your private GitHub api token, and place the script in your path.
+
+Current commands (preceded by `gh` or filename of choice):
+
+   - `create <repo name>`
+      - Creates remote GitHub repo, and if the current directory is a git repository, adds the remote.
---
+++ gh.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+if [ \( "$1" == "create" \) ]
+then
+	if [ \( $# != 2 \) ]
+	then
+		echo "Usage: hub create <repo name>"
+		exit 1
+	fi
+	curl -XPOST -H 'Authorization: token $GH_AUTH_TOKEN' https://api.github.com/user/repos -d '{"name":"'$2'"}' --silent >/dev/null
+	echo 'GitHub repository created.'
+	if [ -d .git ]
+	then
+		git remote add github 'https://github.com/JoahG/'$2'.git'
+		echo 'GitHub remote added.'
+	fi
+fi
